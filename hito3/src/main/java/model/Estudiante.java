8d2b0b17ed3be4e0b5959eed947440df59f55bd7
Stephanie Lazarte <lazarterodasstephanie@gmail.com>
2026-05-08 14:29:14 -0400

    feat: agregar clase Estudiante con id, nombre y puntaje

---
+++ hito3/src/main/java/model/Estudiante.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Estudiante {
+    public int id;
+    public String nombre;
+    public double puntaje;
+
+    public Estudiante(int id, String nombre, double puntaje) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puntaje = puntaje;
+    }
+}
+
