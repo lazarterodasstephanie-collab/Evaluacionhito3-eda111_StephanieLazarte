8f64bbe748ae8cb5e60227655f67ca2f2c52611f
Stephanie Lazarte <lazarterodasstephanie@gmail.com>
2026-05-08 14:24:44 -0400

    feat: crear estructura base del proyecto Spring Boot

---
+++ hito3/src/test/java/com/eda/hito3/Hito3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eda.hito3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hito3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
